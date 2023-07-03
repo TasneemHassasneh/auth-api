a51b909354f821ac092c8bb11164c4dc1526350b
TasneemHassasneh <tasneemhassasneh890@gmail.com>
2023-07-03 14:57:27 +0000

    edit test files

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ require('dotenv').config();
 const app = require('./src/server.js');
 const { db } = require('./src/auth/models');
 
-db.sync().then(() => {
+db.sync({ force: true }).then(() => {
   app.start(process.env.PORT || 3001);
 });
